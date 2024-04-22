aacaf2c8ab4b1eb168c40993087d321802c2c633
fachebot <peatdev@gmail.com>
2024-04-22 13:43:56 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # avail-airdrop-check
 批量检查Avail节点冲突以及轻节点挑战的空投奖励
+
+## 安装依赖
+```bash
+npm install
+```
+
+## 导入助记词
+1. 新增 `mnemonics.txt` 文件
+2. 将 Avail 账户的助记词写入到  `mnemonics.txt` 文件，要求每行一个助记词
+
+## 运行程序
+```bash
+npx ts-node index.ts
+```
---
+++ index.ts
@@ -0,0 +1,85 @@
+import axios from "axios";
+import * as fs from "fs";
+import * as readline from "readline";
+import { Keyring } from "@polkadot/api";
+import { u8aToHex } from "@polkadot/util";
+import { cryptoWaitReady, mnemonicToMiniSecret } from "@polkadot/util-crypto";
+
+let nft_count = 0;
+let claimed_amount = 0;
+const claimed_accounts = new Array<string>();
+
+function getCurrentUnixTimestamp() {
+  const currentUnixTimestamp = Math.floor(Date.now() / 1000);
+  return currentUnixTimestamp;
+}
+
+async function availCheck(mnemonic: string) {
+  const miniSecret = mnemonicToMiniSecret(mnemonic);
+  const privateKey = u8aToHex(miniSecret);
+
+  const keyring = new Keyring({ type: "sr25519" });
+  const keypair = keyring.addFromUri(privateKey);
+  const pk = Buffer.from(keypair.publicKey).toString("hex");
+
+  const ts = getCurrentUnixTimestamp();
+  const message = `Greetings from Avail!\n\nSign this message to check your eligibility. This signature will not cost you any fees.\n\nTimestamp: ${ts}`;
+  const signature = u8aToHex(keypair.sign(message));
+
+  const payload = {
+    account: `0x${pk}`,
+    type: "AVAIL",
+    timestamp: ts,
+    signedMessage: signature,
+  };
+
+  console.info("Checking Account:", keypair.address);
+
+  const url = "https://claim-api.availproject.org/check-rewards";
+  const res = await axios.post(url, payload);
+  if (res.data?.message == "Already Claimed") {
+    const data = res.data.data;
+    claimed_amount += data.claimed_by[0].amount;
+    console.info(
+      `Checking Account: ${keypair.address}, Claimed Amount: ${data.claimed_by[0].amount}`
+    );
+    claimed_accounts.push(mnemonic);
+  } else if (res.data?.message == "Not Eligible") {
+    if (res.data?.nft) {
+      nft_count += 1;
+      console.info(
+        `Checking Account: ${keypair.address}, NFT: ${res.data?.nft.nft_type}`
+      );
+    } else {
+      console.info(`Checking Account: ${keypair.address}, Not Eligible`);
+    }
+  } else {
+    console.info(res.status, JSON.stringify(res.data));
+  }
+
+  return keypair.address;
+}
+
+(async () => {
+  await cryptoWaitReady();
+
+  const rl = readline.createInterface({
+    input: fs.createReadStream("mnemonics.txt"),
+  });
+
+  const mnemonics = new Array<string>();
+  for await (const line of rl) {
+    if (line.length > 0) {
+      mnemonics.push(line);
+    }
+  }
+
+  const addresses = new Array<string>();
+  for (let mnemonic of mnemonics) {
+    const address = await availCheck(mnemonic);
+    addresses.push(address);
+  }
+
+  console.info("NFT Count:", nft_count);
+  console.info("Claimed Amount:", claimed_amount);
+})();
